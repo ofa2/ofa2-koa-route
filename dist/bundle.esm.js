f6e9d24a84e2a7ac46a8d305188c8e8262ffdf11
xinshangshangxin <xinshangshangxin@gmail.com>
2019-09-02 11:44:02 +0800

    chore: build

--- dist/bundle.esm.js
+++ dist/bundle.esm.js
@@ -116,9 +116,10 @@ function lift() {
 
     let policies = this.controllerActionPolicies && this.controllerActionPolicies[`${controllerName}.${actionMethodName}`] || [];
     const funList = [pattern].concat(policies).concat(wrapActionMethod);
-    router.use(addTrace(this.config))[method](...funList);
+    router[method](...funList);
   });
 
+  this.app.use(addTrace(this.config));
   this.app.use(router.routes());
   this.app.use(router.allowedMethods());
 }
